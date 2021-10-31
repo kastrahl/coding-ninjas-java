7050cdb7e7ae7d1a76f2bbe42e1930720fcaa798
kastrahl <56175823+kastrahl@users.noreply.github.com>
2021-10-31 16:11:54 +0530

    Create sumarray.java

---
+++ DSA/oops1/recursion-1/sumarray.java
@@ -0,0 +1,18 @@
+public class Solution {
+	public static int sum(int input[]) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically.
+		*/
+		if(input.length<1){
+            return 0;
+        }
+        int smallarr[]=new int[input.length-1];
+        for(int i=1;i<input.length;i++){
+            smallarr[i-1]=input[i];
+        }
+        return (input[0]+sum(smallarr));
+	}
+}
